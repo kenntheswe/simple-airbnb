0f320da7eeb3def422344c1afa9a0c4954424a27
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-26 01:44:06 +0900

    Added some css to the cards

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  validates :name, presence: :true
+  validates :address, presence: :true
+  validates :price_per_night, numericality: { only_integer: true }
+  validates :number_of_guests, presence: :true
 end
--- app/views/flats/_flat.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! flat, :id, :name, :address, :description, :price_per_night, :number_of_guests, :created_at, :updated_at
-json.url flat_url(flat, format: :json)
